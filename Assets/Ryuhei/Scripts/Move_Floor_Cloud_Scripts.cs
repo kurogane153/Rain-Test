bd27926b1363ae6864f759d07eb13845b10f2ed0
Ryuhei1 <duemachine@gmail.com>
2020-04-09 18:08:10 +0900

    動く床、お邪魔鉄球(あひる)、回転床、落ちる雲(質が悪い)

---
+++ Assets/Ryuhei/Scripts/Move_Floor_Cloud_Scripts.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Floor_Cloud_Scripts : MonoBehaviour {
+
+    public float FallSpeed = -1.0f;
+    private Rigidbody2D rigid;
+    private Vector3 defaultPos;
+    private bool FallSwitch = false;
+
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody2D>();
+        rigid.bodyType = RigidbodyType2D.Static;
+        defaultPos = transform.position;
+	}
+
+    private void FixedUpdate()
+    {
+        if (FallSwitch == true)
+        {
+            rigid.bodyType = RigidbodyType2D.Dynamic;
+
+            transform.position = defaultPos + new Vector3(0, FallSpeed * Time.deltaTime, 0);
+        }
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            FallSwitch = true;
+        }
+    }
+}
---
+++ Assets/Ryuhei/Scripts/Rotation_Floor_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation_Floor_Script : MonoBehaviour {
+
+    public float RotationX = 0.0f;
+    public float RotationY = 0.0f;
+    public float RotationZ = -1.0f;
+    private Rigidbody2D rb; // this Rigidbody2D
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.bodyType = RigidbodyType2D.Static;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // transformを取得
+        Transform myTransform = this.transform;
+
+        // ローカル座標基準で、現在の回転量へ加算する
+        myTransform.Rotate(RotationX, RotationY, RotationZ);
+    }
+}
