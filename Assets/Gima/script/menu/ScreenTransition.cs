2affd5d2359e94c15926374090a73221b233c2f6
kurogane153 <11807067@stu.kbc.ac.jp>
2020-04-27 15:33:44 +0900

    タイトル画面の追加

---
+++ Assets/Gima/script/menu/ScreenTransition.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ScreenTransition : MonoBehaviour {
+
+    float alfa = 0.0f;
+    bool flg = false;
+    private Image Img;
+    private GameObject image_object;
+
+    float speed =0.05f;
+
+    void Start()
+    {
+        image_object = GameObject.Find("Image");
+        Img = image_object.GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        if (flg)
+        {
+            if (alfa <= 1.0f)
+            {
+                Img.color = new Color(Img.color.r, Img.color.g, Img.color.b, alfa);
+                alfa += speed;
+            }
+            else if (alfa > 1.0f)
+            {
+                SceneManager.LoadScene("Rain_Main");
+            }
+        }
+    }
+
+    public void PushButton()
+    {
+        flg = true;
+    }
+}
---
+++ Assets/Gima/script/menu/menu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // UIコンポーネントの使用
+
+public class menu : MonoBehaviour {
+
+    Button GameStart;
+    Button GameEnd;
+
+    void Start()
+    {
+        // ボタンコンポーネントの取得
+        GameStart = GameObject.Find("/Canvas/Game Start").GetComponent<Button>();
+        GameEnd = GameObject.Find("/Canvas/Game End").GetComponent<Button>();
+
+        // 最初に選択状態にしたいボタンの設定
+        GameStart.Select();
+    }
+}
