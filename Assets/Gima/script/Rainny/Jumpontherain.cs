7a0002c465830b2362a8853fe3e5ccf111dab3de
kurogane153 <11807067@stu.kbc.ac.jp>
2020-05-11 17:45:02 +0900

    ジャンプ、パラソル用のビルド

---
+++ Assets/Gima/script/Rainny/Jumpontherain.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumpontherain : MonoBehaviour {
+
+    Animator _animator;
+    private GameObject Player;
+
+    void Start () {
+
+		Player = GameObject.Find("Player");
+        _animator = GetComponent<Animator>();
+
+    }
+
+	void Update () {
+
+        //Player.gameObject.GetComponent<TestJump_ver2>().isGrounded &&
+
+        if (Player.gameObject.GetComponent<TestJump_ver2>().fix)
+        {
+            this.transform.position = Player.transform.position;
+            _animator.SetBool("on the Rain", true);
+        }
+        else if (Player.gameObject.GetComponent<TestJump_ver2>().fix == false)
+        {
+            _animator.SetBool("on the Rain", false);
+        }
+
+	}
+}
