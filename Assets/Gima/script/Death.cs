a7d56533307ec7707c2c017796742368605188f5
kurogane153 <11807067@stu.kbc.ac.jp>
2020-05-25 22:43:51 +0900

    ギミック追加

---
+++ Assets/Gima/script/Death.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death : MonoBehaviour {
+
+    public bool fix = false;
+    GameObject lever;
+
+    void Start()
+    {
+        lever = GameObject.Find("lever");
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (lever.gameObject.GetComponent<Switch>().fix == false)
+        {
+            fix = true;
+        }   
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        fix = false;   
+    }
+}
--- Assets/Gima/script/Delete.cs
+++ Assets/Gima/script/Delete.cs
@@ -14,7 +14,8 @@ public class Delete : MonoBehaviour {
     //当たった時の処理
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == "Rain" || collision.gameObject.tag == "Rain_Red")
+        if (collision.gameObject.tag == "Rain" || collision.gameObject.tag == "Rain_Red"
+            || collision.gameObject.tag == "Rain_White")
         {
             Destroy(collision.gameObject);
         }
