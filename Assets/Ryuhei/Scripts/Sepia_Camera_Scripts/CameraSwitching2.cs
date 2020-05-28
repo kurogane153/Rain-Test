ca2fada8c35480234e50a64ee6ea2a9abc804731
Ryuhei1 <duemachine@gmail.com>
2020-05-28 18:13:50 +0900

    セピアカメラ修正

---
+++ Assets/Ryuhei/Scripts/Sepia_Camera_Scripts/CameraSwitching2.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSwitching2 : MonoBehaviour
+{
+    
+    private GameObject subCamera;       //サブカメラ格納用 
+
+    private GameObject Player;
+
+    //呼び出し時に実行される関数
+    void Start()
+    {
+        //メインカメラとサブカメラをそれぞれ取得
+        subCamera = GameObject.Find("Sub Camera_sepia");
+
+        //サブカメラを非アクティブにする
+        subCamera.SetActive(false);
+
+        Player = GameObject.Find("Player");
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            subCamera.SetActive(true);
+            Debug.Log("sepia");
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            subCamera.SetActive(false);
+            Debug.Log("sepiaOFF");
+        }
+    }
+}
---
+++ Assets/Ryuhei/Scripts/Sepia_Camera_Scripts/ToSubCamer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToSubCamer : MonoBehaviour {
+
+    private GameObject mainCamera;      //メインカメラ格納用
+
+    // Use this for initialization
+    void Start () {
+        
+        mainCamera = GameObject.Find("Main Camera");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.transform.position = new Vector3(mainCamera.transform.position.x
+            , mainCamera.transform.position.y, mainCamera.transform.position.z);
+
+        Debug.Log(this.transform.position.x);
+	}
+}
